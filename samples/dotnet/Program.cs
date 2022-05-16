22982a7848f3e5c8d955dbafd6ae92c4de6865f7
João Paulo Frezzarin <jpfrezzarin@hotmail.com>
2022-05-16 11:20:03 -0300

    Updated method names

--- samples/dotnet/Program.cs
+++ samples/dotnet/Program.cs
@@ -2,8 +2,8 @@
 
 Console.WriteLine("Hello, World!");
 
-int sum = MyNativeLibrary.Sum(1, 2);
-int sub = MyNativeLibrary.Sub(3, 2);
+int sum = MyNativeLibrary.MyLibSum(1, 2);
+int sub = MyNativeLibrary.MyLibSub(3, 2);
 
 Console.WriteLine($"The result of sum is: {sum}");
 Console.WriteLine($"The result of sub is: {sub}");
